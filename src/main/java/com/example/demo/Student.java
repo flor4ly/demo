5ebaffed60ce0339d585ea9a51e64c9ccec19276
Gulira'no Tojimurodova <g.tojimurodova@newuu.uz>
2025-03-19 23:44:42 +0500

    EDITED

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
-public class Student {
+public class Student extends Person {
+    private String studentId;
+
+    public Student(String name, int age, String address, String studentId) {
+        super(name, age, address);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() { return studentId; }
+    public void setStudentId(String studentId) { this.studentId = studentId; }
 }
